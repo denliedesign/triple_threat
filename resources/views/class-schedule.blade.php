0a2a1c057bac68532fec3881e08c0122b5a2fd6e
denliedesign <customdenlie@gmail.com>
2022-08-30 18:12:47 -0500

    new fall sched

--- resources/views/class-schedule.blade.php
+++ resources/views/class-schedule.blade.php
@@ -7,30 +7,33 @@
     </div>
 
     <div class="container">
-        <h1>2021-2022 RECREATIONAL SCHEDULE</h1>
-        <p class="text-white" style="opacity: 80%;">
-            <strong>MONDAY</strong> <br>
-            Hip Hop 4:00-5:00pm (Ages 5-10) <br>
-            <br>
-            <strong>WEDNESDAY</strong> <br>
-            Acro Beginner Level 4:00-5:00pm (Ages 4-9) <br>
-            Acro Intermediate Level 6:00-7:00pm (Ages 10+) <br>
-            Acro Advanced Level 7:00-8:00pm (Ages <br>
-            <br>
-            <strong>THURSDAY</strong> <br>
-            Combo class: Ballet/Tap 4:00-5:00pm (Ages 4-5) <br>
-            Combo class: Ballet/Tap/Jazz 4:00-5:00pm (Ages 6-10) <br>
-            <br>
-            <strong>SATURDAY</strong> <br>
-            Acting 10:00-11:00am <br>
-            Pre- Ballet 10:15- 11:00am (Ages 1.5-3) <br>
-            Combo class: Ballet/Tap 11:00-12:00pm (Ages 3-5) <br>
-            Combo Class: Ballet/Tap/Jazz/Hip Hop 12:00- 2:00pm (Ages 6-10) <br>
-            Combo Class: Ballet/Tap/Jazz/Hip Hop 2:00- 4:00pm (Ages 11+) <br>
-            <br>
-            *COMPANY IS BY AUDITION ONLY. EMAIL FOR MORE INFORMATION <br>
-            *EMAIL US ABOUT HAVING PRIVATE ONE ON ONE TECHNIQUE LESSONS.
-        </p>
+        <div class="d-flex justify-content-center">
+            <img src="/images/class-schedule-2022-2023.jpg" alt="" class="img-fluid">
+        </div>
+{{--        <h1>2021-2022 RECREATIONAL SCHEDULE</h1>--}}
+{{--        <p class="text-white" style="opacity: 80%;">--}}
+{{--            <strong>MONDAY</strong> <br>--}}
+{{--            Hip Hop 4:00-5:00pm (Ages 5-10) <br>--}}
+{{--            <br>--}}
+{{--            <strong>WEDNESDAY</strong> <br>--}}
+{{--            Acro Beginner Level 4:00-5:00pm (Ages 4-9) <br>--}}
+{{--            Acro Intermediate Level 6:00-7:00pm (Ages 10+) <br>--}}
+{{--            Acro Advanced Level 7:00-8:00pm (Ages <br>--}}
+{{--            <br>--}}
+{{--            <strong>THURSDAY</strong> <br>--}}
+{{--            Combo class: Ballet/Tap 4:00-5:00pm (Ages 4-5) <br>--}}
+{{--            Combo class: Ballet/Tap/Jazz 4:00-5:00pm (Ages 6-10) <br>--}}
+{{--            <br>--}}
+{{--            <strong>SATURDAY</strong> <br>--}}
+{{--            Acting 10:00-11:00am <br>--}}
+{{--            Pre- Ballet 10:15- 11:00am (Ages 1.5-3) <br>--}}
+{{--            Combo class: Ballet/Tap 11:00-12:00pm (Ages 3-5) <br>--}}
+{{--            Combo Class: Ballet/Tap/Jazz/Hip Hop 12:00- 2:00pm (Ages 6-10) <br>--}}
+{{--            Combo Class: Ballet/Tap/Jazz/Hip Hop 2:00- 4:00pm (Ages 11+) <br>--}}
+{{--            <br>--}}
+{{--            *COMPANY IS BY AUDITION ONLY. EMAIL FOR MORE INFORMATION <br>--}}
+{{--            *EMAIL US ABOUT HAVING PRIVATE ONE ON ONE TECHNIQUE LESSONS.--}}
+{{--        </p>--}}
     </div>
 
 
